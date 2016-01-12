d734458bd0d2d7c979843673a9f68624986ed029
Pepijn Olivier <olivier.pepijn@gmail.com>
2016-01-12 11:51:41 +0100

    added logged in as into footer
    Signed-off-by: Pepijn Olivier <olivier.pepijn@gmail.com>

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,7 +1,12 @@
 <footer class="container">
     <div class="row">
         <div class="col-md-12">
-            footer
+            @if(!empty($user))
+              Logged in as <div class="label label-info">{{{$user->email}}}</div>
+            @else
+              Not logged in
+            @endif
+
         </div>
     </div>
 </footer>
